b9afc7c2ee27c70e7d5e7d428633ed945a2292a2
yuichiro miyamae <yu1ro.mya@gmail.com>
2025-01-26 13:18:27 +0900

    add: fly config

--- sonic/server/index.ts
+++ sonic/server/index.ts
@@ -2,14 +2,14 @@ import { createApp } from './factory'
 import { rootRoute } from './routes/root'
 import { taskRoute } from './routes/task'
 
-const app = createApp().basePath('/api')
+const app = createApp()
 
 app.use(async (c, next) => {
   await next()
   c.header('X-Powered-By', 'React Router and Hono')
 })
 
-const route = app.route('/', rootRoute).route('/tasks', taskRoute)
+const route = app.route('/api', rootRoute).route('/api/tasks', taskRoute)
 
 export default app
 
