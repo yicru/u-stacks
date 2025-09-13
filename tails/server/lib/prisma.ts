0b07d1640d3a8f6a30df8fec1fbd1de5b40591ec
yuichiro miyamae <yu1ro.mya@gmail.com>
2025-09-13 14:47:08 +0900

    update: ローカルでDBを立てる

--- tails/server/lib/prisma.ts
+++ tails/server/lib/prisma.ts
@@ -1,12 +1,12 @@
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@server/generated/prisma/client'
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient
 }
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
