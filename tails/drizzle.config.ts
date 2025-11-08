ce692b37e50ecc420e7dc8336e0365a7b9d94387
yuichiro miyamae <yu1ro.mya@gmail.com>
2025-11-08 13:56:34 +0900

    update: migrate to dotenvx and update deployment scripts

--- tails/drizzle.config.ts
+++ tails/drizzle.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'drizzle-kit'
 
-import 'dotenv/config'
-
 export default defineConfig({
   out: './drizzle',
   schema: ['./server/db/schema.ts', './server/db/auth-schema.ts'],
